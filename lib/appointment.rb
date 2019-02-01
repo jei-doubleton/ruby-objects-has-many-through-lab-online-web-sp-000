abb77b1a6eec27c73e8d69d1b9474813f3049e2f
J. Singleton <jennifer.singleton@gmail.com>
2019-02-01 04:03:37 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,7 +3,7 @@ class Appointment
   attr_accessor :date, :patient, :doctor
   @@all = []
 
-  def initialize(patient, doctor, date)
+  def initialize(date, doctor, patient)
 binding.pry
     @date = date
     @patient = patient
