dc7cfb9c3ce58c77d817707462cf4066ba50472b
J. Singleton <jennifer.singleton@gmail.com>
2019-02-01 03:18:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,13 @@
 #connector
 class Song
+  attr_accessor :name, :artist, :genre
+
   @@all = []
 
   def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
     @@all << self
   end
 
